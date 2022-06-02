86ca5b4fd6b9f2a717dad5d302fb14590884dc4e
Junior <manoel8485@gmail.com>
2022-06-01 20:07:00 -0400

    Post da requisição

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function create(){
+        return view('welcome');
+    }
+    public function store(Request $request){
+        Post::create($request->all());
+        return view('welcome');
+    }
+    public function index(){
+        $requisicao = Post::all();
+        return view('welcome',compact('requisicao'));
+    }
 }
