e8001d1c211b00eb6e6638692bcc2a0b7ea25267
Junior <manoel8485@gmail.com>
2022-06-03 22:02:53 -0400

    Ultimate

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,46 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<style>
+    
+*{margin: 10px; padding:0; box-sizing:border-box;}
+.content{display:flex; margin: 20;}
+/*.content{d-fle justify-content: right 20;}*/
+/*body{
+    font-family: Arial, Helvetica, sans-serif;
+    background-image: linear-gradient(45deg, cyan, yellow);
+}*/
+body{
+    background: linear-gradient(to left, rgb(20,147,220), rgb(17,54,71));
+    color: rgb(12, 12, 12);
+    text-align: left;
+}
+
+.rTable{width: 100%; text-align: center;}
+
+    .rTable thead{background:slategray; font-weight: bold; color:#fff;}
+    .rTable tbody tr:nth-child(2n){background: #ccc;}
+    .rTable th , .rTable td{padding: 7px 0;}
+
+@media screen and (max-width: 480px){
+    .content{width: 94%;}
+    
+    .rTable thead{display:none;}
+    .rTable tbody td{display: flex; flex-direction: column; }
+}
+
+@media only screen and (min-width: 1200px){
+    .content{width: 90%;}
+    .rTable tbody tr td:nth-child(1){width:10%;}
+    .rTable tbody tr td:nth-child(2){width:30%;}
+    .rTable tbody tr td:nth-child(3){width:20%;}
+    .rTable tbody tr td:nth-child(4){width:10%;}
+    .rTable tbody tr td:nth-child(5){width:30%;}
+}
+
+
+</style>
+
 </head>
 <body>
     <!--Botão de sair do Login-->
@@ -44,9 +84,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                        
                     </td>
                 </tr>
     
@@ -56,9 +94,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                   
                     </td>
                 </tr>
                 <tr>
@@ -67,9 +103,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                    
                     </td>
                 </tr>
                 <tr>
@@ -78,9 +112,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                 
                     </td>
                 </tr>
                 <tr>
@@ -89,9 +121,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                   
                     </td>
                 </tr>
                 <tr>
@@ -100,9 +130,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                        
                     </td>
                 </tr>
                 <tr>
@@ -111,9 +139,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                       
                     </td>
                 </tr>
                 <tr>
@@ -122,9 +148,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                       
                     </td>
                 </tr>
                 <tr>
@@ -133,37 +157,20 @@
                     <td></td>
                     <td></td>
                     <td>
-                        @foreach($requisicao as $requisicoes)
-                            <p>{{$requisicoes->$categoria}}</p>
-                        @endfoeach
+                       
                     </td>
                 </tr>
             </tbody>
         </table>
 
         <tbody>
-        <!-- Buscando as infomação no banco com while.
-            <php
-                while($user_data = mysqli_fetch_assoc($result))
-                {
-                    echo "<tr>";
-                    echo "<tr>".$user_data['codigo']."<tr>";
-                    echo "<tr>".$user_data['secretaria']."<tr>";
-                    echo "<tr>".$user_data['pendentes']."<tr>";
-                    echo "<tr>".$user_data['realizado']."<tr>";
-                    echo "<tr>".$user_data['total']."<tr>";
-                    echo "</tr>";
-                }
-                
-            ?>
-        -->
 
         <!--Botão de voltar-->
         
         <div class="row">
-            <li class="nav-item">
+            
                 <a type="button" class="btn btn-primary "nav-link href="index.html">Voltar</a>
-            </li>
+            
         </div>
     </div>
     </div> <!-- fim do row -->
